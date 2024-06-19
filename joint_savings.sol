7be73b9890129828e0da5a6fe0b78122e2e09b34
SERENA SHKABARI <serenashkabari@yahoo.com>
2024-06-19 18:28:34 +0200

    changed the fallback function

--- joint_savings.sol
+++ joint_savings.sol
@@ -88,6 +88,6 @@ contract JointSavings {
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    function() external payable {
+    fallback() external payable {
     }
 }
